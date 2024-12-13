0d472c5ef351992cb6f594d7cd37f770d633d222
unknown <david.mitrea@gmail.com>
2024-12-13 11:06:15 +0200

    other small fixes (deleted class Recomandations service and made a static method in that file

--- ambergreen/AiManagement/recomandationsManagement/RecomandationService.py
+++ ambergreen/AiManagement/recomandationsManagement/RecomandationService.py
@@ -1,38 +1,30 @@
 from ollama import chat
 
-class RecomandationService:
-    def getRecomandations(self, data):
-        response = chat(
-            model='llama3.2',
-            messages=[
-                {
-                    'role': 'user',
-                    'content': f"{data['year']}, {data['building']}: {data['consumption']}."
-                               f"Based on this data, identify the categories where the building can improve. You can search the internet to see the building, its size and purpose"
-                               f"For each category, suggest actionable steps the building managers can take to reduce its environmental impact, and estimate the potential percentage reduction achievable for each action."
-                               f"I want you to format the reponse in this way:"
-                               f"Recomandations for energy: "
-                               f"..."
-                               f"Recomandations for gas: "
-                               f"..."
-                               f"Recomandations for water: "
-                               f"..."
-                               f"I dont want you to use any characters like '*', '#'"
-                               f"Please provide your suggestions, and begin each one with a - character."
-                               f"I dont want you to write anything else after the recomandations such as 'Please note ...'"
-                }
-            ],
-            options={"stream": False,
-                     "temperature": 0.2}
-        )
-        return response['message']['content']
 
-# recomandationService = RecomandationService()
-# data = {
-#     'year': 2024,
-#     'building': 'Primaria Cluj-Napoca - Calea Motilor 3, Cluj-Napoca 400001',
-#     'consumption': 'total_electrical_consumption = 350 kWh total_water_consumption = 30 m³ total_gas_consumption = 122 m³',
-# }
-# recomandations = recomandationService.getRecomandations(data)
-#
-# print(recomandations)
+def getRecomandations(data):
+    response = chat(
+        model='llama3.2',
+        messages=[
+            {
+                'role': 'user',
+                'content': f"{data['month']}: {data['co2_emissions']}."
+                           f"Based on this data, identify the categories where the building can improve."
+                           f"For each category, suggest actionable steps the building managers can take to reduce its environmental impact, and estimate the potential percentage reduction achievable for each action."
+                           f"I want you to format the reponse in this way:"
+                           f"Recomandations for energy: "
+                           f"..."
+                           f"Recomandations for gas: "
+                           f"..."
+                           f"Recomandations for water: "
+                           f"..."
+                           f"I dont want you to use any characters like '*', '#'"
+                           f"Please provide your suggestions, and begin each one with a - character."
+                           f"I dont want you to write anything else after the recomandations such as 'Please note ...'"
+                           f"I dont want you to write recomandations for anything else just these 3 categories (energy, gas, water)"
+            }
+        ],
+        options={"stream": False,
+                 "temperature": 0}
+    )
+    return response['message']['content']
+
